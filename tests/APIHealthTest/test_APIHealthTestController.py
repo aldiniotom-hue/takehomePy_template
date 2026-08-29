25ef686f4ead1ab608b0a31436e56db9bd0032f5
Tomas <aldinio.tom@gmail.com>
2026-08-29 10:51:11 -0300

    .

--- tests/APIHealthTest/test_APIHealthTestController.py
+++ tests/APIHealthTest/test_APIHealthTestController.py
@@ -15,3 +15,17 @@ async def test_post_health_test(client: AsyncClient):
     )
 
     assert response.status_code == 201
+
+
+@pytest.mark.anyio
+async def test_database_is_empty(client: AsyncClient):
+    response = await client.post(
+        "/api/healthtest/getHealthTest",
+        json={
+            "datetime": datetime.datetime.now().isoformat(),
+            "message": "",
+        },
+    )
+
+    data = response.json()
+    assert data.lenght() == 0
