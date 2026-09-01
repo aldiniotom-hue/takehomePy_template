87f475b6b8bcad3d6d05dd58025be11d0f649337
Tomas <aldinio.tom@gmail.com>
2026-09-01 14:21:04 -0300

    added Id to BaseUser in schemas

--- routes/schemas/schemas.py
+++ routes/schemas/schemas.py
@@ -9,11 +9,13 @@ class APIHealthBase(BaseModel):
 
 
 class BaseUser(BaseModel):
+    id: int
     email: EmailStr = Field(max_length=120)
 
 
 class UserCreate(BaseUser):
     password: str = Field(min_length=8)
+
 
 class Token(BaseModel):
     access_token: str
